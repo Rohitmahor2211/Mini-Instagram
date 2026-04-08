624bd42f7c129776af032450c38568d8fb31b93b
RohitM <rohitmahor022@gmail.com>
2026-04-08 18:16:53 +0530

    email smtp change

--- server/config/email.js
+++ server/config/email.js
@@ -3,8 +3,9 @@ const nodemailer = require("nodemailer");
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // use SSL
+    port: 587,
+    secure: false,
+    family: 4, // 👈 force IPv4
     auth: {
         user: process.env.Email_user_account,
         pass: process.env.Email_APP_KEY,
